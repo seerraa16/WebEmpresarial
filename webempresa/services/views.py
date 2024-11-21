ece2333a3f347d649c04a7ef9acdf4e86c3947ec
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-21 18:36:39 +0100

    cambios de chill

--- webempresa/services/views.py
+++ webempresa/services/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
-# Create your views here.
+def services(request):
+ return render(request, "services/services.html")
--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -19,8 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 urlpatterns = [
  path('', include('core.urls')),
+
+ path('services/', include('services.urls')),
  path('admin/', admin.site.urls),
 ]
+
 if settings.DEBUG:
  from django.conf.urls.static import static
  urlpatterns += static(settings.MEDIA_URL,

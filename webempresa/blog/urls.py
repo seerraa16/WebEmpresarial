a3760244db6d3552bf8997ad97ad82ad8c8e03ce
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-23 14:02:02 +0100

    Creando las vistas del Blog (2)

--- webempresa/blog/urls.py
+++ webempresa/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
- path('', views.blog, name="blog"),
+    path('', views.blog, name="blog"),
+    path('category/<int:category_id>/', views.category, name="category"),
 ]

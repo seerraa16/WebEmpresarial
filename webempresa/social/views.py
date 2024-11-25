dac4214eed62ec8c8b9fd6bdf587f3b52ec05a5b
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-25 15:16:36 +0100

    ya funciona pero se ve rara la lampara

--- webempresa/social/views.py
+++ webempresa/social/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import Service
+from .models import Link  # Cambiar Services por Link
 
 # Create your views here.
 
 def social(request):
-    social = social.objects.all()
-    return render(request, "social/social.html",
-{'social':social})
+    # Cambiar social.objects.all() a Link.objects.all()
+    social_links = Link.objects.all()
+    return render(request, "social/social.html", {'social': social_links})

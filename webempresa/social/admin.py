0887b1a6642604b329e71581fac67df1bc410e8a
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-25 16:02:30 +0100

    procesadores de contexto

--- webempresa/social/admin.py
+++ webempresa/social/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.contrib import admin
+from .models import Link
+# Register your models here.
+class LinkAdmin(admin.ModelAdmin):
+ pass
+admin.site.register(Link, LinkAdmin) 
 # Register your models here.

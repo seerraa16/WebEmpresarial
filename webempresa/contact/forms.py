0678053e28fe2136b41f9f823f7a8593036939a6
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-27 11:19:45 +0100

    Fusionando el formulario

--- webempresa/contact/forms.py
+++ webempresa/contact/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+
+# Create your forms here.
 class ContactForm(forms.Form):
- name = forms.CharField(
-    label="Nombre", required=True)
- email = forms.EmailField(
-    label="Email", required=True)
- content = forms.CharField(
-    label="Contenido", required=True, widget=forms.Textarea()
-)
+    name = forms.CharField(label="Nombre", required=True, min_length=3, max_length=100,
+                           widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escribe tu nombre'}))
+    email = forms.EmailField(label="Email", required=True, min_length=3, max_length=100,
+                             widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Escribe tu email'}))
+    content = forms.CharField(label="Contenido", required=True, min_length=10, max_length=1000,
+                              widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Escribe tu mensaje'}))
